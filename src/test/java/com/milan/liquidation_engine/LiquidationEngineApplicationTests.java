8da27f15970e605a1e83ea1e12b43e507c52ba77
Milan Tyagi <milantyagi2004@gmail.com>
2026-05-21 21:32:48 +0530

    initilize the project

---
+++ src/test/java/com/milan/liquidation_engine/LiquidationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milan.liquidation_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidationEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
